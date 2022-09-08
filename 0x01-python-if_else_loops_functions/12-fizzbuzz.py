1c214b0b64aa33bc3870aed454ab12166ba8d4c3
T-LES <lucaselat@gmail.com>
2022-09-08 16:56:56 +0000

    Adding a Python script

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for i in range(100):
         s = i + 1
-        if s % 3 == 0:
-            print("Fizz", end=" ")
+        if s % 3 == 0 and s % 5 == 0:
+            print("FizzBuzz", end=" ")
         elif s % 5 == 0:
             print("Buzz", end=" ")
-        elif s % 3 == 0 and s % 5 == 0:
-            print("FizzBuzz", end=" ")
+        elif s % 3 == 0):
+            print("Fizz", end=" ")
         else:
             print("{:d}".format(s), end=" ")
