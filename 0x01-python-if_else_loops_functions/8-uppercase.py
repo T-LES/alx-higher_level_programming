dc817e7044c92e13e0da8838698ae303da23c19e
T-LES <lucaselat@gmail.com>
2022-09-08 15:31:09 +0000

    Adding a Python script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for w in range(str):
+    for w in str:
         if ord(w) >= 97 and ord(w) <= 122:
             w = ord(w) - (ord('z') - ord('Z'))
         print("{:c}".format(w), end='')
